5a7e78d42062ac32bfd99618bc7ec029e56f9b07
Alghorani <49647299+Alghorani@users.noreply.github.com>
2019-12-11 21:32:51 +0800

    Update Sparkle.java

--- SWM-Assignment-master/Diamond Hunter/src/com/neet/DiamondHunter/Entity/Sparkle.java
+++ SWM-Assignment-master/Diamond Hunter/src/com/neet/DiamondHunter/Entity/Sparkle.java
@@ -8,16 +8,7 @@ import java.awt.Graphics2D;
 import com.neet.DiamondHunter.Manager.Content;
 import com.neet.DiamondHunter.TileMap.TileMap;
 
-public class Sparkle extends Entity {
-	
-	private boolean remove;
-	
-	public Sparkle(TileMap tm) {
-		super(tm);
-		animation.setFrames(Content.SPARKLE[0]);
-		animation.setDelay(5);
-		width = height = 16;
-	}
+
 	
 	public boolean shouldRemove() {
 		return remove;
