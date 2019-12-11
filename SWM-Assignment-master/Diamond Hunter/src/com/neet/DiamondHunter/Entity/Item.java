a7fca4e411b6c71e934e34647c198ac53b0662db
Alghorani <49647299+Alghorani@users.noreply.github.com>
2019-12-11 21:32:26 +0800

    Update Item.java

--- SWM-Assignment-master/Diamond Hunter/src/com/neet/DiamondHunter/Entity/Item.java
+++ SWM-Assignment-master/Diamond Hunter/src/com/neet/DiamondHunter/Entity/Item.java
@@ -17,7 +17,12 @@ public class Item extends Entity{
 	private int type;
 	public static final int BOAT = 0;
 	public static final int AXE = 1;
-	
+		public Item(TileMap tm) {
+		super(tm);
+		type = -1;
+		width = height = 16;
+		cwidth = cheight = 12;
+	}
 
 	
 	public void setType(int i) {
